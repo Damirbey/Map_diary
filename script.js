9ffbe16137b6dd0dc951432f6a1a6dff014f9431
Damirbey <dydamirbey@gmail.com>
2023-10-27 15:43:30 -0400

    Implemented adding activity functionality

--- script.js
+++ script.js
@@ -3,16 +3,33 @@ const activityInput = document.getElementById('activity');
 const durationInput = document.getElementById('duration');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+const activitiesContainer = document.querySelector('.activities'); 
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 class Activity{
     date = new Date();
-    id = new (Date.now()+'').slice(-10);
+    id = (Date.now() + '').slice(-10);
     activityName;
     activityDuration;
     
     constructor(name, duration){
         this.activityName = name;
         this.activityDuration = duration;
+    }
+
+    getDateDescription(){
+        return daysOfWeek[this.date.getDay()] + ' ' +this.date.getDate() + ' ' + months[this.date.getMonth()] +',' 
+                    + this.date.getFullYear()
+    }
+}
+class ActivityRestored extends Activity{
+    constructor(name,duration,date,id){
+        super();
+        this.activityName = name;
+        this.activityDuration = duration;
+        this.date = new Date(date);
+        this.id = id;
     }
 }
 
@@ -23,6 +40,9 @@ class App{
     activities = [];
     constructor(){
         this._getPosition();
+
+        this._getLocalStorage();
+
         overlay.addEventListener('click', this.closeModal);
         form.addEventListener('submit',this._addNewActivity.bind(this));
     }
@@ -64,10 +84,12 @@ class App{
     }
 
     _showForm(mapEvent){
-        console.log('HERE!')
         this.#mapEvent = mapEvent;
         form.classList.remove('hidden');
         activityInput.focus();
+    }
+    _hideForm(){
+        form.classList.add('hidden');
     }
 
     _addNewActivity(e){
@@ -78,8 +100,57 @@ class App{
         }
         let activity = new Activity(activityInput.value,durationInput.value);
         this.activities.push(activity);
-        form.classList.add('hidden');
+
+        this._renderActivity(activity);
+
+        this._setLocalStorage();
+
+        this._hideForm();
     }
+    _setLocalStorage(){
+        localStorage.setItem('activities', JSON.stringify(this.activities));
+    }
+    _getLocalStorage(){
+        if(!localStorage.getItem('activities')){
+            this.showMessage('Your activities not found!');
+            return;
+        }
+
+        this.activities = JSON.parse(localStorage.getItem('activities'));
+        this.activities = this.activities.map(activity=>{
+            return new ActivityRestored(activity.activityName,activity.activityDuration,activity.date,activity.id)
+        })
+        this.activities.map(activity=>{
+            this._renderActivity(activity);
+        })
+    }
+    _reset(){
+        localStorage.removeItem('activities');
+    }
+    _renderActivity(activity){
+       let activityDate = activity.getDateDescription()
+       let activitiyHtml = `<div class="activity">
+                        
+                <div class="activity_item">
+                    <i class="fa fa-thumb-tack activity_icon" aria-hidden="true"></i>
+                    <p class="activity_text">${activity.activityName}</p>
+                </div>
+                      
+                <div class="activity_item">
+                    <i class="fa fa-clock-o activity_icon" aria-hidden="true"></i>
+                    <p class="activity_text">${activity.activityDuration} hours</p>
+                </div>
+
+                <div class="activity_item">
+                    <i class="fa fa-calendar-check-o activity_icon" aria-hidden="true"></i>
+                    <p class="activity_text">${activityDate}</p>
+                </div>
+                      
+            </div>`
+
+        activitiesContainer.insertAdjacentHTML('afterend',activitiyHtml);
+    }
+
     inputsValid(){
         if(activityInput.value.length == 0 || durationInput.value < 0 || durationInput.value.length == 0){
             return false;
@@ -92,6 +163,7 @@ class App{
         overlay.classList.add('hidden');
         modal.classList.add('hidden');
     }
+
     showMessage(message){
         overlay.classList.remove('hidden');
         modal.classList.remove('hidden');
